b4d4ee598a9e2ddcc7d925f4d5caba23bca7d0a2
ibrahim achemine <achemine2007@gmail.com>
2025-11-27 12:07:37 +0100

    Update Tylor_series.c

--- TP3/Tylor_series.c
+++ TP3/Tylor_series.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int n, x;
-    float result ;
+    // Variables declaration
+    int n, x, i = 1;
+    double result = 1.0, factorial = 1;
 
+    // getting user input and verifying it
+    do{
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Enter the exponent: ");
     scanf("%d", &x);
+    } while (n <= 0);
 
-    for (int fac = 1; fac <= n-1 ; fac++)
-    {
-        result += result + fac;
-        
-    }
-    printf("The result of the Taylor series is: %f\n", result);
+    // Calculating the Taylor series
+    do {
+        result += pow(x, i) / (factorial*=i);
+        i++;
+    } while (i < n);
+
+    // Displaying the result
+    printf("The Taylor series result is: %.15lf\n", result);
     return 0;
 }
