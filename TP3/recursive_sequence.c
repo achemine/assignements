06efd2c125daed04f2d1f48cdb61c72a85c00449
ibrahim achemine <achemine2007@gmail.com>
2025-11-29 13:16:42 +0100

    t

--- TP3/recursive_sequence.c
+++ TP3/recursive_sequence.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("hello")
+    const float epsilon = 0.0001;
+    float A;
+    float Un, Un1;
+    int pointeur = 0;
+    // Getting user input
+    printf("Enter the value of A: ");
+    scanf("%f", &A);
 
+    // Initial term of the sequence
+    Un = A;
+    Un1 = (Un + (A / Un)) / 2;
+    while ( fabs(Un1 - Un) < epsilon) 
+    {   
+        Un1 = (Un + (A / Un)) / 2;
+        Un = Un1;
+        printf("iteration %d: %f\n", pointeur, Un);
+        pointeur++;
+    }
+    printf(" converged after %d iterations\n", pointeur);
+    printf("the square root of %d is approximately: %f\n", A, Un1);
     return 0; 
 }
