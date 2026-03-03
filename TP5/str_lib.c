9d3b39e31b695953c48b8625edac8aee5faec6de
ibrahim <achemine2007@gmail.com>
2026-03-03 13:45:01 +0100

    last changes

--- TP5/str_lib.c
+++ TP5/str_lib.c
@@ -1,57 +1,63 @@
 #include <stdio.h>
 
 // function's prototypes
-
+int my_strlen(char *str);
+void my_strcpy(char *dest, char *src);
+void my_strcat(char *dest, char *src);
+int my_strcmp(char *str1, char *str2);
+void my_strrev(char *str);
 
 // main code
-int main () 
+int main()
 {
-    char s1 [200] , s2 [200] , dest [400];
-    //getting s1 and s2 from user input
-    printf (" Enter string 1: ");
-    fgets (s1 , 200 , stdin );
-    s1 [ my_strlen ( s1 ) - 1] = '\0';
+    char s1[200], s2[200], dest[400];
+    // getting s1 and s2 from user input
+    printf(" Enter string 1: ");
+    fgets(s1, 200, stdin);
+    s1[my_strlen(s1) - 1] = '\0';
 
-    printf (" Enter string 2: ");
-     fgets (s2 , 200 , stdin );
-     s2 [ my_strlen ( s2 ) - 1] = '\0';
+    printf(" Enter string 2: ");
+    fgets(s2, 200, stdin);
+    s2[my_strlen(s2) - 1] = '\0';
 
-    //first functions testing 
-    printf ("\n=== my_strlen ===\ n");
-    printf (" Length of \"% s \": %d\n", s1 , my_strlen ( s1 )) ;
-    printf (" Length of \"% s \": %d\n", s2 , my_strlen ( s2 )) ;
+    // first functions testing
+    printf("\n=== my_strlen ===\n");
+    printf(" Length of \"% s \": %d\n", s1, my_strlen(s1));
+    printf(" Length of \"% s \": %d\n", s2, my_strlen(s2));
 
-    //second functions testing
-     printf ("\n=== my_strcpy ===\ n");
-    my_strcpy ( dest , s1 );
-    printf (" Copied : \"% s\"\ n", dest ) ;
+    // second functions testing
+    printf("\n=== my_strcpy ===\n");
+    my_strcpy(dest, s1);
+    printf(" Copied : \"% s\"\n", dest);
 
-    //third functions testing
-    printf ("\n=== my_strcat ===\ n");
-    my_strcpy ( dest , s1 ); // start fresh with a copy of s1
-    my_strcat ( dest , " "); // append a space
-    my_strcat ( dest , s2 ); // append s2
-    printf (" Concatenated : \"% s \"\ n", dest );
+    // third functions testing
+    printf("\n=== my_strcat ===\n");
+    my_strcpy(dest, s1);  // start fresh with a copy of s1
+    my_strcat(dest, " "); // append a space
+    my_strcat(dest, s2);  // append s2
+    printf(" Concatenated : \"% s \"\n", dest);
 
-    //fourth functions testing
-    printf ("\n=== my_strcmp ===\ n");
-    int cmp = my_strcmp ( s1 , s2 );
-    if ( cmp == 0) printf (" \"% s\" == \"% s \"\ n", s1 , s2 ) ;
-    else if ( cmp > 0) printf (" \"% s\" > \"% s \"\ n", s1 , s2 );
-    else printf (" \"% s\" < \"% s \"\ n", s1 , s2 ) ;
+    // fourth functions testing
+    printf("\n=== my_strcmp ===\n");
+    int cmp = my_strcmp(s1, s2);
+    if (cmp == 0)
+        printf(" \"% s\" == \"% s \"\n", s1, s2);
+    else if (cmp > 0)
+        printf(" \"% s\" > \"% s \"\n", s1, s2);
+    else
+        printf(" \"% s\" < \"% s \"\n", s1, s2);
 
-    //fifth functions testing
-    printf ("\n=== my_strrev ===\ n");
-    my_strcpy ( dest , s1 ); // work on a copy to keep s1 unchanged
-    my_strrev ( dest ) ;
-    printf (" Reversed \"% s \": \"% s \"\ n", s1 , dest );
+    // fifth functions testing
+    printf("\n=== my_strrev ===\n");
+    my_strcpy(dest, s1); // work on a copy to keep s1 unchanged
+    my_strrev(dest);
+    printf(" Reversed \"% s \": \"% s \"\n", s1, dest);
     return 0;
 }
 
-
 // function's definitions:
 // phase 1:
-int my_strlen( char *str)
+int my_strlen(char *str)
 {
     int length = 0;
     while (str[length] != '\0')
@@ -63,21 +69,14 @@ int my_strlen( char *str)
 
 // phase 2
 void my_strcpy(char *dest, char *src)
-{   
-    if (my_strlen(dest) < my_strlen(src))
+{
+    int i = 0;
+    while (src[i] != '\0')
     {
-        return -1;
+        dest[i] = src[i];
+        i++;
     }
-    else
-    {
-        int i = 0;
-        while (src[i] != '\0')
-        {
-            dest[i] = src[i];
-            i++;
-        }
-        dest[i] = '\0';
-    }
+    dest[i] = '\0';
 }
 
 // phase 3
